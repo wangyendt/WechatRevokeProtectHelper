311dea845183aaa815872a4e16e798f2dcefd901
Ye Wang <905317742@qq.com>
2019-11-15 18:36:23 +0800

    Update options.py

--- options.py
+++ options.py
@@ -9,14 +9,14 @@
 @time: 2019/11/11 17:55
 """
 
-ME_UID = '@94d9aa6bd1133aab30b6dd037988abb6'
+ME_UID = '@d28b2e8f9de96d1541d2f569d4645f46'
 ME_NAME = '王也 Wayne'
 LISTENING_FRIENDS_NICKNAME = [':-D', 'Landiy', 'Suerte']
-LISTENING_FRIENDS_REMARK_NAME = ['魏锦华']
+LISTENING_FRIENDS_REMARK_NAME = ['魏锦华', '彭满萍']
 LISTENING_GROUPS = [
     '跑跑卡丁车', '元旦北海道小队-平时王者自律', '再跑一圈就卸载',
     '爸爸 妈妈 王也', '试验群', '思想生活交流群-NDT码农', '爱吃饭，爱生活，好嗨哟',
-    '幸福家庭', 'leet code / tf进阶学习'
+    '幸福家庭', 'leet code / tf进阶学习', '2019下半年愿望实现群'
 ]
 DEFAULT_REPLY = '[对方已离线] 默认回复'
 
